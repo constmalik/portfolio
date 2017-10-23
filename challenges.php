d7d5a75fdb592fa15ad2fc5325edf4c4d183e312
Malik Robinson <maliklorenzorobinson@gmail.com>
2017-10-23 19:07:06 -0400

    added challenges page

--- challenges.php
+++ challenges.php
@@ -1,7 +1,69 @@
 <?php
 include('inc/header.php');
 ?>
+<h1>Challenges</h1>
+<div class="challenges-content">
+<!-- when styling weekly challenge list items user display inline so they can appear next to each other-->
+  <ul class="weekly-list">
+    <li>
+      <p>Week 5 Challenges</p>
+      <ul>
+        <li><a href="https://codepen.io/maliklorenzo96/pen/pwmJQZ" target="_blank">Challenges 1-4</a></li>
+        <li><a href="https://codepen.io/maliklorenzo96/pen/LLowLe" target="_blank">Challenge 5</a></li>
+      </ul>
+    </li>
+    <li>
+      <p>Week 6 Challenges</p>
+      <ul>
+        <li><a href="https://codepen.io/maliklorenzo96/pen/RZwOwr" target="_blank">Easy Challenge</a></li>
+      </ul>
+    </li>
+    <li>
+      <p>Week 7 Challenges</p>
+      <ul>
+        <li><a href="https://codepen.io/collection/XMJgvz/" target="_blank">All Challenges</a></li>
+      </ul>
+    </li>
+    <li>
+      <p>Week 8 Challenges</p>
+      <ul>
+        <li><a href="https://codepen.io/collection/DwgBaj/" target="_blank">All Challenges</a></li>
+      </ul>
+    </li>
+    <li>
+      <p>Week 9 Challenges</p>
+      <ul>
+        <li><a href="#" target="_blank">Contact React App</a></li>
+      </ul>
+    </li>
+    <li>
+      <p>Week 10 Challenges</p>
+      <ul>
+        <li><a href="#" target="_blank">Team Builder React App</a></li>
+      </ul>
+    </li>
+    <li>
+      <p>Week 12 Challenges</p>
+      <ul>
+        <li><a href="#" target="_blank">Comment React App</a></li>
+        <li><a href="#" target="_blank">Guessing Game React App</a></li>
+      </ul>
+    </li>
+    <li>
+      <p>Week 14 Challenges</p>
+      <ul>
+        <li><a href="#" target="_blank">Shuffled Deck PHP Challenges</a></li>
+      </ul>
+    </li>
+    <li>
+      <p>Week 16 Challenges</p>
+      <ul>
+        <li><a href="#" target="_blank">C# Challenges</a></li>
+      </ul>
+    </li>
+  </ul>
 
+</div>
 <?php
 include('inc/footer.php');
 ?>
