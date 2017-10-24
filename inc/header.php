d4ba51dd9c1a2653ee5a8506fdf5646ba434a232
Malik Robinson <maliklorenzorobinson@gmail.com>
2017-10-24 14:55:02 -0400

    fixed it

--- inc/header.php
+++ inc/header.php
@@ -10,7 +10,7 @@
       <header id="header">
         <nav id="nav">
           <ul id="nav-links">
-            <li <?php if($pageTitle == "Home") {echo "id='active'";}?>><a href="index.php">Home</a></li>
+            <li><a href="index.php">Home</a></li>
             <li><a href="about.php">About</a></li>
             <li><a href="projects.php">Projects</a></li>
             <li><a href="resume.php">Resume</a></li>
